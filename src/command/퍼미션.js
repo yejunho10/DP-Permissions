02d5afc458cae9b00ddc013404caa70089ae0bcd
AMD_5900X <98728918+yejunho10@users.noreply.github.com>
2022-10-18 17:19:43 +0900

    style: 퍼미션.js

--- src/command/퍼미션.js
+++ src/command/퍼미션.js
@@ -45,49 +45,49 @@ const 퍼미션command = new SlashCommandBuilder()
                             { name: "DP-SimplePrefix", value: "DSP" },
                             { name: "DP-SimpleShop", value: "DSS" }
                     )))
-.addSubcommand((subcommand) =>
-    subcommand
-        .setName("이지")
-        .setDescription("이지 플러그인 퍼미션을 조회합니다.")
-        .addStringOption((option) =>
-            option
-                .setName("플러그인")
-                .setDescription("퍼미션을 조회할 플러그인을 입력합니다.")
-                .setRequired(true)
-                .setChoices(
-                        { name: "DP-EasyEntitySpawn", value: "DEES" },
-                        { name: "DP-EasyMessages", value: "DEM" },
-                        { name: "DP-EasyWarning", value: "DEW" },
-                        { name: "DP-EasyWarp", value: "DEWP" }
-                )))
-.addSubcommand((subcommand) =>
-    subcommand
-        .setName("레전더리")
-        .setDescription("레전더리 플러그인 퍼미션을 조회합니다.")
-        .addStringOption((option) =>
-            option
-                .setName("플러그인")
-                .setDescription("퍼미션을 조회할 플러그인을 입력합니다.")
-                .setRequired(true)
-                .setChoices(
-                        { name: "DP-LegendaryCash", value: "DLC" },
-                        { name: "DP-LegendaryEnderChest", value: "DLEC" },
-                        { name: "DP-LegendaryGun", value: "DLG" },
-                        { name: "DP-LegendaryLogger", value: "DLL" },
-                        { name: "DP-LegendaryRPG", value: "DLR" },
-                        { name: "DP-LegendaryStorage", value: "DLS" }
-                )))
-.addSubcommand((subcommand) =>
-    subcommand
-        .setName("코어")
-        .setDescription("코어 플러그인 퍼미션을 조회합니다.")
-        .addStringOption((option) =>
-            option
-                .setName("플러그인")
-                .setDescription("퍼미션을 조회할 플러그인을 입력합니다.")
-                .setRequired(true)
-                .setChoices(
-                        { name: "DPP-Core", value: "DPPC" }
-                )))
+    .addSubcommand((subcommand) =>
+        subcommand
+            .setName("이지")
+            .setDescription("이지 플러그인 퍼미션을 조회합니다.")
+            .addStringOption((option) =>
+                option
+                    .setName("플러그인")
+                    .setDescription("퍼미션을 조회할 플러그인을 입력합니다.")
+                    .setRequired(true)
+                    .setChoices(
+                            { name: "DP-EasyEntitySpawn", value: "DEES" },
+                            { name: "DP-EasyMessages", value: "DEM" },
+                            { name: "DP-EasyWarning", value: "DEW" },
+                            { name: "DP-EasyWarp", value: "DEWP" }
+                    )))
+    .addSubcommand((subcommand) =>
+        subcommand
+            .setName("레전더리")
+            .setDescription("레전더리 플러그인 퍼미션을 조회합니다.")
+            .addStringOption((option) =>
+                option
+                    .setName("플러그인")
+                    .setDescription("퍼미션을 조회할 플러그인을 입력합니다.")
+                    .setRequired(true)
+                    .setChoices(
+                            { name: "DP-LegendaryCash", value: "DLC" },
+                            { name: "DP-LegendaryEnderChest", value: "DLEC" },
+                            { name: "DP-LegendaryGun", value: "DLG" },
+                            { name: "DP-LegendaryLogger", value: "DLL" },
+                            { name: "DP-LegendaryRPG", value: "DLR" },
+                            { name: "DP-LegendaryStorage", value: "DLS" }
+                    )))
+    .addSubcommand((subcommand) =>
+        subcommand
+            .setName("코어")
+            .setDescription("코어 플러그인 퍼미션을 조회합니다.")
+            .addStringOption((option) =>
+                option
+                    .setName("플러그인")
+                    .setDescription("퍼미션을 조회할 플러그인을 입력합니다.")
+                    .setRequired(true)
+                    .setChoices(
+                            { name: "DPP-Core", value: "DPPC" }
+                    )))
 
 export default 퍼미션command.toJSON();
